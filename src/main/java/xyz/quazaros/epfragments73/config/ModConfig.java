aa571a9d784d047a3ccc3c62f6c4eaedb6743b9b
BowserTheBoss <jake.busekrus@gmail.com>
2026-02-02 13:37:00 -0500

    Added configs

---
+++ src/main/java/xyz/quazaros/epfragments73/config/ModConfig.java
@@ -0,0 +1,16 @@
+package xyz.quazaros.epfragments73.config;
+
+public class ModConfig {
+    public int ruined_chance = 1;
+    public int withered_chance = 1;
+    public int golden_chance = 1;
+    public int gusty_chance = 1;
+    public int ancient_chance = 1;
+    public int dark_chance = 1;
+    public int wealthy_chance = 1;
+    public int sandy_chance = 1;
+
+    public void validate() {
+
+    }
+}
--- src/main/java/xyz/quazaros/epfragments73/item/util/LootTables.java
+++ src/main/java/xyz/quazaros/epfragments73/item/util/LootTables.java
@@ -9,16 +9,26 @@ import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.util.Identifier;
 import org.lwjgl.opengl.AMDOcclusionQueryEvent;
+import xyz.quazaros.epfragments73.config.ConfigManager;
 import xyz.quazaros.epfragments73.item.ModItems;
 import xyz.quazaros.epfragments73.main;
 
 public class LootTables {
     public static void registerLootTables() {
+        int ruined = ConfigManager.get().ruined_chance;
+        int withered = ConfigManager.get().withered_chance;
+        int golden = ConfigManager.get().golden_chance;
+        int gusty = ConfigManager.get().gusty_chance;
+        int ancient = ConfigManager.get().ancient_chance;
+        int dark = ConfigManager.get().dark_chance;
+        int wealthy = ConfigManager.get().wealthy_chance;
+        int sandy = ConfigManager.get().sandy_chance;
+
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
             if (source.isBuiltin() && key.getValue().equals(Identifier.of("minecraft", "entities/elder_guardian"))) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .with(ItemEntry.builder(ModItems.RUINED_EYE).weight(1))
+                        .with(ItemEntry.builder(ModItems.RUINED_EYE).weight(ruined))
                         .with(EmptyEntry.builder().weight(2));
 
                 tableBuilder.pool(poolBuilder);
@@ -27,33 +37,33 @@ public class LootTables {
             if (source.isBuiltin() && key.getValue().equals(Identifier.of("minecraft", "entities/wither"))) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .with(ItemEntry.builder(ModItems.WITHERED_EYE).weight(1));
+                        .with(ItemEntry.builder(ModItems.WITHERED_EYE).weight(withered));
 
                 tableBuilder.pool(poolBuilder);
             }
 
             if (source.isBuiltin() && key.getValue().equals(Identifier.of("minecraft", "gameplay/piglin_bartering"))) {
                 tableBuilder.modifyPools(poolBuilder -> {
-                    poolBuilder.with(ItemEntry.builder(ModItems.GOLDEN_EYE).weight(8));
+                    poolBuilder.with(ItemEntry.builder(ModItems.GOLDEN_EYE).weight(golden * 8));
                 });
             }
 
             if (source.isBuiltin() && key.getValue().equals(Identifier.of("minecraft", "chests/trial_chambers/reward_ominous_unique"))) {
                 tableBuilder.modifyPools(poolBuilder -> {
-                    poolBuilder.with(ItemEntry.builder(ModItems.GUSTY_EYE).weight(3));
+                    poolBuilder.with(ItemEntry.builder(ModItems.GUSTY_EYE).weight(gusty * 3));
                 });
             }
 
             if (source.isBuiltin() && key.getValue().equals(Identifier.of("minecraft", "archaeology/trail_ruins_rare"))) {
                 tableBuilder.modifyPools(poolBuilder -> {
-                    poolBuilder.with(ItemEntry.builder(ModItems.ANCIENT_EYE).weight(2));
+                    poolBuilder.with(ItemEntry.builder(ModItems.ANCIENT_EYE).weight(ancient * 2));
                 });
             }
 
             if (source.isBuiltin() && key.getValue().equals(Identifier.of("minecraft", "chests/ancient_city"))) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .with(ItemEntry.builder(ModItems.DARK_EYE).weight(1))
+                        .with(ItemEntry.builder(ModItems.DARK_EYE).weight(dark))
                         .with(EmptyEntry.builder().weight(14));
 
                 tableBuilder.pool(poolBuilder);
@@ -62,8 +72,8 @@ public class LootTables {
             if (source.isBuiltin() && key.getValue().equals(Identifier.of("minecraft", "chests/woodland_mansion"))) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .with(ItemEntry.builder(ModItems.WEALTHY_EYE).weight(1))
-                        .with(EmptyEntry.builder().weight(3));
+                        .with(ItemEntry.builder(ModItems.WEALTHY_EYE).weight(wealthy))
+                        .with(EmptyEntry.builder().weight(2));
 
                 tableBuilder.pool(poolBuilder);
             }
@@ -71,7 +81,7 @@ public class LootTables {
             if (source.isBuiltin() && key.getValue().equals(Identifier.of("minecraft", "chests/desert_pyramid"))) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .with(ItemEntry.builder(ModItems.SANDY_EYE).weight(1))
+                        .with(ItemEntry.builder(ModItems.SANDY_EYE).weight(sandy))
                         .with(EmptyEntry.builder().weight(4));
 
                 tableBuilder.pool(poolBuilder);
