824a7b84a769ae883e3f245f20dcc272808c8556
YourName <Jake.Busekrus@gmail.com>
2026-05-08 12:37:20 -0400

    Changed the version file location

--- Forge/1.21.11/src/main/java/xyz/quazaros/epfragments73/item/util/ParchmentMappings.java
+++
@@ -1,9 +0,0 @@
-package xyz.quazaros.epfragments73.item.util;
-
-import net.minecraft.resources.Identifier;
-
-public class ParchmentMappings {
-    public static Identifier getPath(String id, String thing) {
-        return Identifier.m_445280_(id, thing);
-    }
-}
